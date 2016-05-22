8bb492f11d891ea1838de8b9d1990e43c9381512
Benji <benji@benjicraft.info>
2016-05-22 19:04:02 +0100

    Fixed never ending loop problem

--- bot.js
+++ bot.js
@@ -4,9 +4,10 @@ var mybot = new Discord.Client();
 
 mybot.on("message", function(message){
 	if(message.content === "!cmds")
-		mybot.reply(message, "!cmds: Get a list of all commands");
-		mybot.reply(message, "!help: Send a message to a member of support to get help")
-		mybot.reply(message, "!about: Learn all about the bot and the creator")
+		mybot.reply(message, "Prefix messages with !")
+		mybot.reply(message, "cmds: Get a list of all commands");
+		mybot.reply(message, "help: Send a message to a member of support to get help")
+		mybot.reply(message, "about: Learn all about the bot and the creator")
 	if(message.content === "!help")
 		mybot.reply(message, "has requested help. Please now move to the help/support channel, a support agent will be with you shortly");
 	if(message.content === "!about")
