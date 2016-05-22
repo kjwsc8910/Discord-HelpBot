c7ba766d282d68768116fd4b719447b091312bdb
Benji <benji@benjicraft.info>
2016-05-22 18:51:01 +0100

    Added a cmds command

--- bot.js
+++ bot.js
@@ -4,9 +4,11 @@ var mybot = new Discord.Client();
 
 mybot.on("message", function(message){
 	if(message.content === "!cmds")
-		mybot.reply(message, "List Coming Soon!");
+		mybot.reply(message, "!cmds: Get a list of all commands");
+		mybot.reply(message, "!help: Send a message to a member of support to get help")
+		mybot.reply(message, "!about: Learn all about the bot and the creator")
 	if(message.content === "!help")
-		mybot.reply(message, "@support, Help requested in general");
+		mybot.reply(message, "has requested help. Please now move to the help/support channel, a support agent will be with you shortly");
 	if(message.content === "!about")
 		mybot.reply(message, "This bot is made by BenjiSoft (Benji Lewis) for the open-source community. All code is availiable at: https://github.com/benjisoft/Discord-HelpBot and is coded in Javascript using the discord.js library availiable at: https://github.com/hydrabolt/discord.js/")
 });
