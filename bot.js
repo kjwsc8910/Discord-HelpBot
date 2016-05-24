e0b9c55aa0d55cf1c93fe321141eb987b1fbbad8
kjwsc8910 <05kj@sky.com>
2016-05-24 19:36:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ mybot.on("message", function(message){
 	if(message.content === "!help")
 		mybot.reply(message, "has requested help. Please now move to the help/support channel, a support agent will be with you shortly");
 	if(message.content === "!about")
-		mybot.reply(message, "This bot is made by BenjiSoft (Benji Lewis) for the open-source community. All code is availiable at: https://github.com/benjisoft/Discord-HelpBot and is coded in Javascript using the discord.js library availiable at: https://github.com/hydrabolt/discord.js/")
+		mybot.reply(message, "This bot is made by KJHard -KJ Hardwere- (KJ Woledgesinclair!) for the open-source community. All code is availiable at: https://github.com/benjisoft/Discord-HelpBot and is coded in Javascript using the discord.js library availiable at: https://github.com/hydrabolt/discord.js/")
 });
 
 mybot.loginWithToken("MTg0NzM1NjQ5ODUxNTA2Njg5.CiYv-w.DwFcASCH1Ll_h5OsJZN4Gt50PKI");
