9c317adc7856eadf74ac4536613aa615b3b0e657
kjwsc8910 <05kj@sky.com>
2016-05-25 19:11:06 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,13 @@ mybot.on("message", function(message){
 	if(message.content === "!cmds")
 		mybot.reply(message, "!cmds. This will show a list of commands")
 	if(message.content === "!cmds")
-		mybot.reply(message, "!info. This will show Info on my creators KJ ")
+		mybot.reply(message, "!info. This will show Info on my creators KJ ''meet my creators!''")
 	if(message.content === "!cmds")
 		mybot.reply(message, "!chatbanon. turns on automatic ban from messages in chat")
 	if(message.content === "!info")
-		mybot.reply(message, "My creators are KJ Woledgesinclair and benjiman lewis. benjiman lewis had created the files to make me work for discor, And most importantly KJ Woledgesinclair who coded me to work and talk")
+		mybot.reply(message, "My creators are KJ Woledgesinclair and benjiman lewis. benjiman lewis had created the files to make me work for discord, And most importantly KJ Woledgesinclair who coded me to work and talk.")
+	if(message.content === "!info")
+		mybot.reply(message, " KJ has made a way to make me say multiple thing per command in a random order -Quite smart- and ben has uesd that idear in his helper bot, go and check it out!")")
 	
 });
 
